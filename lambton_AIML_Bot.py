a91a9e63f9141ace972109464c607a6b6ec2f05d
Reddy1990 <Reddy1990@users.noreply.github.com>
2022-04-09 18:37:24 -0400

    Added more intents

--- lambton_AIML_Bot.py
+++ lambton_AIML_Bot.py
@@ -2,14 +2,14 @@
 """
 Created on Fri Apr  8 16:51:15 2022
 
-@author: injav
+@author: Group 4
 """
 
 import logging
 import random
 import string
 import sys
-
+from datetime import datetime as date
 import nltk
 import pandas as pd
 import yaml
@@ -18,33 +18,39 @@ from sklearn.linear_model import LogisticRegression
 from telegram.ext import Updater, MessageHandler, Filters
 
 student_id = ''
+today = date.today().strftime("%A")
+print(today)
 
 student_information = {
     "C01": {
         "name": "John",
         "exam": "April 14, 2022",
         "grades": "CBD: 89%\nALML:95%",
-        "fee": "$200"
+        "fee": "$200",
     },
     "C02": {
         "name": "Dan",
         "exam": "April 14, 2022",
         "grades": "CBD: 90%\nALML:95%",
-        "fee": "$100"
+        "fee": "$100",
     },
     "C03": {
         "name": "Michelle",
         "exam": "April 14, 2022",
         "grades": "CBD: 79%\nALML:95%",
-        "fee": "$150"
+        "fee": "$150",
     },
     "C04": {
         "name": "Rosi",
         "exam": "April 14, 2022",
         "grades": "CBD: 84%\nALML:95%",
-        "fee": "$230"
+        "fee": "$230",
+       
     }
 }
+
+subjects =   {"Monday": "Into to AI at 1:00 PM to 4.30 PM", "Tuesday": "CBD at 12:30 PM to 4.30 PM" , "Wednesday": "Python at 12:30 PM to 4.30 PM", "Thursday": "DS at 08:00 AM to 12.30 PM","Friday":"Communication at 11.30 AM to 4:00 PM", "Saturday": "No classes today", "Sunday": "No classes today"}
+
 
 
 def LemTokens(tokens):
@@ -88,7 +94,7 @@ def bot_initialize(update, context):
                 student_id = user_response or 'C01'
                 resp = str(random.choice(responses[1]['response'])).format(
                     username=student_information.get(student_id, {}).get('name'))
-                what_we_do = """Hi! Here is what I can do!!! \n1. I can give you your Grades\n2. College Fee\n3. Exam Dates.\n\n4. or Type goodbye to Exit."""
+                what_we_do = """Hi! Here is what I can do!!! \n1. I can give you your Grades\n2. College Fee\n3. Exam Dates.\n4 Today class details \n\n5. or Type goodbye to Exit."""
                 update.message.reply_text(resp + '\n' + what_we_do)
                 return
 
@@ -115,6 +121,13 @@ def bot_initialize(update, context):
                                                                             exam_date=student_info.get('exam'))
                 update.message.reply_text(resp)
                 return
+            
+            if user_intent == "time":
+                resp = str(random.choice(responses[14]['response'])).format(username=student_info.get('name'),
+                                                                            class_subj=subjects.get(today))
+                update.message.reply_text(resp)
+                return
+
 
             if user_intent == 'thankyou':
                 resp = random.choice(responses[10]['response'])
@@ -144,42 +157,22 @@ if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     data = [
         ["Hi", 0], ["Hello", 0], ["Hey", 0], ["Heya", 0],
-        ["Suggest for birthday", 2], ["For Birthday", 2], ["I want some flowers for Birthday", 2],
-        ["What type of flowers to give on Birthday?", 2], ["Suggest something for Birthday", 2],
-        ["I want to give it to someone for Birthday", 2],
-        ["Suggest for anniversary", 3], ["For Anniversary", 3], ["I want some flowers for Anniversary", 3],
-        ["What type of flowers to give on Anniversary?", 3], ["Suggest something for Anniversary", 3],
-        ["I want to give it to someone for Anniversary", 3],
-        ["Suggest for congratulations", 4], ["For Congratulations", 4],
-        ["I want some flowers for Congratulating someone", 4],
-        ["What type of flowers to give for Congratulations?", 4], ["Suggest something for Congratulations", 4],
-        ["I want to give it to someone for Congratulating them", 4],
-        ["Suggest for wedding", 5], ["For Wedding", 5], ["I want some flowers for Wedding", 5],
-        ["What type of flowers to give on Wedding?", 5], ["Suggest something for Wedding", 5],
-        ["I want to give it to someone for Wedding", 5],
-        ["Suggest for sorry", 6], ["For Sorry", 6], ["I want some flowers for saying Sorry", 6],
-        ["What type of flowers to give for saying Sorry?", 6], ["Suggest something for saying Sorry", 6],
-        ["I want to give it to someone for saying Sorry", 6],
-        ["Suggest for miss you", 7], ["For Miss You", 7], ["Missing Someone", 7],
-        ["I want some flowers for Miss You", 7], ["What type of flowers to give on Miss You?", 7],
-        ["Suggest something for Miss You", 7], ["I want to give it to someone for Miss You", 7],
-        ["Suggest for get well soon", 8], ["For Get Well Soon", 8], ["I want some flowers for Get Well Soon", 8],
-        ["What type of flowers to give for Get Well Soon?", 8], ["Suggest something for Get Well Soon", 8],
-        ["I want to give it to someone for Get Well Soon", 8],
         ["Goodbye", 9], ["Byebye", 9], ["Bye", 9],
         ["Thanku", 10], ["Thank You", 10], ["Thanks", 10], ["Thanks a lot", 10], ["Thank you very much", 10],
         ["Thank you so much", 10],
         ["grade", 11], ['grades', 11], ['marks', 11],
         ["fee", 12], ['college fee', 12], ['semester fee', 12],
-        ["date", 13], ['exam date', 13], ['semester date', 13]
+        ["subject time", 14], ['class time', 14], ['today class time', 14], ['today subject class time', 14],
+        ["final exam date", 13], ['exam date', 13], ['semester date', 13],
     ]
 
     responses = {
         0: {"intent": "greetings", "response": ['Hi Dear', 'Hi', 'Hello', 'Nice to see you', ]},
         1: {"intent": "studentid", "response": ['Welcome to Lambton {username}!']},
         11: {"intent": "grade", "response": ['Hi {username}, your grades are as follows\n {grades}']},
-        12: {"intent": "fee", "response": ['Hi {username}, your total college_fee is {college_fee}']},
+        12: {"intent": "fee", "response": ['Hi {username}, your total college_fee is {college_fee}. Please visit this URL to pay the fee https://www.lambtoncollege.ca/custom/LambtonApps/Payments/SecurePay.aspx']},
         13: {"intent": "date", "response": ['Hi {username}, your exam date is {exam_date}']},
+        14: {"intent": "time", "response": ['Hi {username}, you have {class_subj} . please use this URL to join class https://moodle.cestarcollege.com/moodle/mod/url/view.php?id=1003405']},
         9: {"intent": "goodbye", "response": ['Goodbye', 'Byebye', 'Bye', 'Have a good day']},
         10: {"intent": "thankyou",
              "response": ['You\'re welcome.', 'No problem.', 'No worries.', ' My pleasure.', 'Glad to help.']}}
