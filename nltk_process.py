cee03bf9f25b597687244bf93d4d3ec9109d488b
Reddy1990 <Reddy1990@users.noreply.github.com>
2022-04-11 23:16:48 -0400

    Refactored code

---
+++ nltk_process.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:10:28 2022
+
+@author: Group 4
+"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import nltk
+import string
+from bot_responses import BotResponses
+import random
+
+tfidf = TfidfVectorizer(tokenizer=self.Normalize, stop_words='english')
+
+class NLTKProcess:
+
+    lr = LogisticRegression()
+    remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+    lemmer = nltk.stem.WordNetLemmatizer()
+    botResponse = BotResponses()
+
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer(tokenizer=self.Normalize, stop_words='english')
+    
+
+
+    def performLogisticRegression( self, dataset ):
+            x = dataset['Text']
+            y = dataset['Intent']
+            X = self.vectorizer.fit_transform(x)
+            self.lr.fit(X, y)
+            X_test = ["exam"]
+            prediction = self.lr.predict(self.vectorizer.transform(X_test))
+            self.lr.predict_proba(self.vectorizer.transform(X_test))
+
+    
+    def LemTokens(self, tokens):
+        for token in tokens:
+            return self.lemmer.lemmatize(token)
+
+    def Normalize(self, text):
+            word_tokenize = nltk.word_tokenize(text.lower().translate(self.remove_punct_dict))
+            return self.LemTokens(word_tokenize)
+
+
+    def response(self,user_response):
+        text_test = [user_response]
+        X_test = self.vectorizer.transform(text_test)
+        prediction = self.lr.predict(X_test)
+        reply = random.choice(self.botResponse.responses[prediction[0]]['response'])
+        return reply
+
+
+    # To get intent
+    def intent(self,user_request):
+        text_intent = [user_request]
+        X_test_intent = self.vectorizer.transform(text_intent)
+        predicted_intent = self.lr.predict(X_test_intent)
+        intent_predicted = self.botResponse.responses[predicted_intent[0]]['intent']
+        return intent_predicted
+
+            
